22cded3741760e68d8ec5e6452b0129e4e9caa08
ranwawa <274544338>
2020-09-27 18:38:03 +0800

    feat(config): 配置信息调整

--- cloudfunctions-aliyun/common/api/config.js
+++ cloudfunctions-aliyun/common/api/config.js
@@ -14,9 +14,10 @@ const test = {
 };
 const prod = {
   alhc: {
-    baseUrl: "http://www.alihuocang.com/",
-    accessToken: "202008291546_dce61a33962345acb3835b180cbb2c3c",
+    baseUrl: "http://www.tzwchb.com/",
+    accessToken: "202008061255_13ac5ae0e819464298256ba608f759db",
+    spId: "5f705f19fcfc0c000111d3b2",
   },
 };
 
-module.exports = test;
+module.exports = prod;
--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,7 +1,7 @@
 export default {
   uniCloudSpace: {
     provider: "aliyun",
-    spaceId: "e2e873a7-3cfe-4a2d-8c8c-a985b2e5c69c",
-    clientSecret: "ivPbpR9/JfXrS+U1f5c3rw==",
+    spaceId: "66108fd9-83f0-4fda-9219-f9fe3a6a377e",
+    clientSecret: "BF6ATHIcf8W8kOr4gDgCSA==",
   },
 };
